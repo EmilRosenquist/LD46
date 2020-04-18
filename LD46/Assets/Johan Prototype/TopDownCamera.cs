b06e7df602d219440a44ac3ab8fe12b1ea60daa7
Johan Laven <lars.johan.urban.laven@gmail.com>
2020-04-18 16:45:05 +0200

    zoom is najs

--- LD46/Assets/Johan Prototype/TopDownCamera.cs
+++ LD46/Assets/Johan Prototype/TopDownCamera.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class TopDownCamera : MonoBehaviour
 {
     [SerializeField] private GameObject dudeToFollow;
-    [SerializeField] private float deadZone = 5.0f;
+    [SerializeField] private float closest = 5.0f;
+    [SerializeField] private float furthest = 35.0f;
     [SerializeField] private float rotAngle = 45.0f;
+    
+    private float deadZone = 5.0f;
     private Vector3 ofset;
 
     // Start is called before the first frame update
     void Start()
     {
         ofset = gameObject.transform.position - dudeToFollow.transform.position;
+        SetZoom(30);
+
         if (deadZone < 0) ofset *= -1;
     }
 
@@ -20,14 +25,20 @@ public class TopDownCamera : MonoBehaviour
     void Update()
     {
         FollowDude();
+        float scrollVal = Input.mouseScrollDelta.y;
+        if (scrollVal != 0)
+        {
+            Zoom(-scrollVal);
+        }
     }
     
     void FollowDude()
     {
-    /*
-     * Follows character with the camera
-     * center stays within ofset from the dude
-     */
+        /*
+         * Follows character with the camera
+         * center stays within ofset from the dude
+         */
+        deadZone = CalculateDeadZone();
         Vector3 centerPos = gameObject.transform.position - ofset;
         Vector3 deltaPos = centerPos - dudeToFollow.transform.position;
         if (deltaPos.magnitude > deadZone)
@@ -36,4 +47,25 @@ public class TopDownCamera : MonoBehaviour
             gameObject.transform.position = dudeToFollow.transform.position + newDPos + ofset;
         }
     }
+
+    void Zoom(float zoomVal)
+    {
+        /*Zooms on scroll*/
+        float CurrentZoom = ofset.magnitude;
+        float targetZoom = Mathf.Clamp(CurrentZoom + Mathf.Clamp(zoomVal, -1, 1), closest, furthest);
+        ofset = ofset.normalized * targetZoom;
+    }
+
+    void SetZoom(float dist)
+    {
+        /*Set Zoom level*/
+        float targetZoom = Mathf.Clamp(dist, closest, furthest);
+        ofset = ofset.normalized * targetZoom;
+    }
+
+    float CalculateDeadZone()
+    {
+        return ofset.magnitude / 6;
+    }
+
 }
