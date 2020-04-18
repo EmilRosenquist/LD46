fceb8419f1992f713234e98beb8cca956c0eb1f9
EmilRosenquist <emil.rosenquist@gmail.com>
2020-04-18 12:52:35 +0200

    Add crafting hut

---
+++ LD46/Assets/Script/CraftingHut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingHut : MonoBehaviour, IMouseInteractable
+{
+    public int craftRange = 10;
+    public string GetText()
+    {
+        return "Use this hut to craft stuff.";
+    }
+
+    public void OnPress(Vector3 position, Inventory inventory)
+    {
+        if(Vector3.Distance(transform.position, position) < craftRange)
+        {
+            Debug.Log("Start craft away!");
+        }
+        else
+        {
+            Debug.Log("To far away to craft");
+        }
+    }
+}
