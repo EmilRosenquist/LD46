06ca1fd3f17222a3e3b280eb641d82ac4103fcc3
Johan Laven <lars.johan.urban.laven@gmail.com>
2020-04-19 00:48:18 +0200

    fixed linkes and stuff

---
+++ LD46/Assets/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private int level = 1;
+    private float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
