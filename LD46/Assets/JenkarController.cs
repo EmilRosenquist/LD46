94fc0cba863e11327b903c6105a5b5957d45433b
Johan Laven <lars.johan.urban.laven@gmail.com>
2020-04-18 23:52:44 +0200

    Jänkare byter riktning om de sitter fast

--- LD46/Assets/JenkarController.cs
+++ LD46/Assets/JenkarController.cs
@@ -9,11 +9,15 @@ public class JenkarController : MonoBehaviour
     [SerializeField] private bool roaming = true;
     private float reachLim = 0.5f;
     private CharacterController cc;
+    private float timeStuck = 1.0f;
+    private float stuckTimer = 1.0f;
+    Vector3 stuckPos;
 
     Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
+        stuckPos = transform.position;
         cc = GetComponent<CharacterController>();
         if(roaming) target = GenerateTarget();
     }
@@ -23,6 +27,12 @@ public class JenkarController : MonoBehaviour
     {
         if (roaming)
         {
+            stuckTimer -= Time.deltaTime;
+            if (stuckTimer < 0)
+            {
+                stuckTimer = 1.0f;
+                if (checkIfStuck()) target = GenerateTarget();
+            }
             if (Vector3.Distance(transform.position, target) < reachLim)
             {
                 target = GenerateTarget();
@@ -53,6 +63,17 @@ public class JenkarController : MonoBehaviour
         print(newTarget);
 
         return newTarget;
+    }
+
+    bool checkIfStuck()
+    {
+        if(Vector3.Distance(stuckPos, transform.position) < timeStuck)
+        {
+            print("STUCK");
+            return true;
+        }
+        stuckPos = transform.position;
+        return false;
     }
 
 
