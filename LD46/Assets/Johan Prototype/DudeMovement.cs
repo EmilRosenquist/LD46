baa7eca19b06dbc82d4fc8054b6fa9df0dcf749a
Johan Laven <lars.johan.urban.laven@gmail.com>
2020-04-18 09:57:29 +0200

    simple move

--- LD46/Assets/Johan Prototype/DudeMovement.cs
+++ LD46/Assets/Johan Prototype/DudeMovement.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class DudeMovement : MonoBehaviour
 {
     [SerializeField] private float maxVel = 2.0f;
-    [SerializeField] private float sprintMultiplier = 1.5f;
+    private CharacterController cc; 
+    private Vector3 heightPoint = new Vector3(0, 3, 0);
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cc = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveDir = GetMoveDirection();
+        RotateDude(moveDir);
+        GetMovePos(moveDir);
+        FallDown();
     }
 
-    Vector3 getMoveDirection()
+    Vector3 GetMoveDirection()
     {
-        Vector3 dir = new Vector3();
+        // Returns a normalized direction of movement
+        Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         return dir;
+    }
+
+    void RotateDude(Vector3 moveDir)
+    {
+        gameObject.transform.forward = moveDir;
+    }
+    
+    void GetMovePos(Vector3 moveDir)
+    {
+        cc.Move(moveDir * maxVel * Time.deltaTime);
+    }
+
+    void FallDown()
+    {
+        
     }
 }
