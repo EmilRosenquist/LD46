3a9d964993344cd3010815aade2d884949d81f43
Johan Laven <lars.johan.urban.laven@gmail.com>
2020-04-18 09:37:13 +0200

    camera follow, dude inc

---
+++ LD46/Assets/Johan Prototype/DudeMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeMovement : MonoBehaviour
+{
+    [SerializeField] private float maxVel = 2.0f;
+    [SerializeField] private float sprintMultiplier = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 getMoveDirection()
+    {
+        Vector3 dir = new Vector3();
+        return dir;
+    }
+}
---
+++ LD46/Assets/Johan Prototype/TopDownCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject dudeToFollow;
+    [SerializeField] private float deadZone = 5.0f;
+    [SerializeField] private float rotAngle = 45.0f;
+    private Vector3 ofset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ofset = gameObject.transform.position - dudeToFollow.transform.position;
+        if (deadZone < 0) ofset *= -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowDude();
+    }
+    
+    void FollowDude()
+    {
+    /*
+     * Follows character with the camera
+     * center stays within ofset from the dude
+     */
+        Vector3 centerPos = gameObject.transform.position - ofset;
+        Vector3 deltaPos = centerPos - dudeToFollow.transform.position;
+        if (deltaPos.magnitude > deadZone)
+        {
+            Vector3 newDPos = deltaPos.normalized * deadZone;
+            gameObject.transform.position = dudeToFollow.transform.position + newDPos + ofset;
+        }
+    }
+}
